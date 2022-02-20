97e4df59c58a2b5cdfb8d11ddc8ad532acefdba3
newDevel2toro <vasea.badragan@gmail.com>
2022-02-20 10:59:15 +0100

    starting project

---
+++ script.js
@@ -0,0 +1,16 @@
+var color1 = document.getElementById("color1");
+var color2 = document.getElementById("color2");
+var body =  document.querySelector("body");
+var h3 = document.querySelector("h3");
+
+function setBackgroundColor() {
+        body.style.background = "linear-gradient(to right, "+color1.value+", "+color2.value+")";
+        h3.textContent = body.style.background; 
+	}	
+	
+
+color1.addEventListener("input", setBackgroundColor);
+color2.addEventListener("input", setBackgroundColor);	
+
+
+
